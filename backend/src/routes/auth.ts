a43984e42e6b1c91929d12148cf0e61b5f47dd7f
shrutijmhatre <shrutijmhatre@gmail.com>
2024-04-04 19:39:49 +0400

    be - logout end point

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -15,4 +15,11 @@ authRoutes.get(
   },
 );
 
+authRoutes.post("/logout", (req: Request, res: Response) => {
+  res.cookie("auth_token", "", {
+    expires: new Date(0),
+  });
+  res.send();
+});
+
 export default authRoutes;
